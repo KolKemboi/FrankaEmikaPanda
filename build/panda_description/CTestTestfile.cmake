af05b61a5ed2835499680a891dd1c74ff00f2f63
KolKemboi <kolkemboi@gmail.com>
2025-08-22 17:26:15 +0300

    Add gitignore for colcon build artifacts

--- build/panda_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kol/Documents/panda/src/panda_description
-# Build directory: /home/kol/Documents/panda/build/panda_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
